3275967bf13875e6884f03ad559b91ae0fc0b8f2
roman <romanlekh@gmail.com>
2018-03-26 21:53:36 +0300

    bonigarcia is configured

--- src/main/java/libs/ParentTest.java
+++ src/main/java/libs/ParentTest.java
@@ -1,83 +1,78 @@
 package libs;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import static libs.ConfigData.*;
 
 @RunWith(Parameterized.class)
 public class ParentTest {
-  public RemoteWebDriver driver;
-    WebDriver iE, fireFox, chrome;
-  public Logger log = Logger.getLogger(getClass());
+    public WebDriver driver;
+    public Logger log = Logger.getLogger(getClass());
 
-  @Rule 
-  public TestName name = new TestName();
+    @Rule
+    public TestName name = new TestName();
 
-  @Parameterized.Parameters
-  public static Collection data() {
-   return Arrays.asList(new String[][] { 
-    //{ "fireFoxString" }
-    // ,
-    { "chromeString" }
-    //,
-    //{ "ieString" }
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new String[][]{
+                {getCfgValue("BROWSER")}
+        });
 
-    });
-  }
+    }
 
-  
-  public ParentTest(String browser) throws MalformedURLException {
-      //-------------Configuration driver path for WIN-----------------
+    public ParentTest(String browser) throws MalformedURLException {
+        //-------------Configuration driver path for WIN-----------------
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("safebrowsing.enabled", "true");
+
       /*System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
       System.setProperty("webdriver.ie.driver", "C:\\IEDriverServer.exe");
       System.setProperty("webdriver.edge.driver", "C:\\MicrosoftWebDriver.exe");*/
-   if (browser.equals("fireFoxString")) {
-    this.driver = new FirefoxDriver();
-      /* DesiredCapabilities capability = DesiredCapabilities.firefox();
-       this.driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-               capability);*/
-  } else if (browser.equals("chromeString")) {
-     this.driver = new ChromeDriver();
+        if (browser.equals("fireFox")) {
+            WebDriverManager.firefoxdriver().setup();
+            this.driver = new FirefoxDriver();
+        }
+        else if (browser.equals("chrome"))
+        {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setExperimentalOption("prefs", prefs);
+            options.addArguments("disable-infobars");
+            this.driver = new ChromeDriver(options);
+            driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+        }
+        else if (browser.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            this.driver = new EdgeDriver();
+        }
 
-        /*DesiredCapabilities capability = DesiredCapabilities.chrome();
-        this.driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                capability);*/
-   }
-   else if (browser.equals("ieString")) {
-       this.driver = new InternetExplorerDriver();
-
-        /*DesiredCapabilities capability = DesiredCapabilities.ie();
-        this.driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-                capability);*/
-   }
-
-   log.info(" ----- " + driver.getClass() + " -------" );
-   
-   
-
-  }
- 
- @Before
- public void setUp(){
-     log.info("Test - " +  name.getMethodName() + " - started");
- }
+        log.info(" ----- " + driver.getClass() + " -------" );
 
 
 
+    }
+
+    @Before
+    public void setUp(){
+        log.info("Test - " +  name.getMethodName() + " - started");
+    }
 
 
 }
